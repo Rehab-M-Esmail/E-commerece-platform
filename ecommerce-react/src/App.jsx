6140c7f0eb7c8fd33f23f52a92ace63dafe38ea6
shahdhoss <s-shahd.fouda@zewailcity.edu.eg>
2024-12-27 00:14:12 +0200

    added products list/ search results

--- ecommerce-react/src/App.jsx
+++ ecommerce-react/src/App.jsx
@@ -3,12 +3,17 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Confirmation from './Components/Confirmation' 
-
+import Login from './Components/Login'
+import Product from './Components/Product'
+import ProductsList from './Components/ProductsList'
 function App() {
 
   return (
     <>
-      <Confirmation/>
+      {/* <Login/> */}
+      {/* <Confirmation/> */}
+      {/* <Product/> */}
+      <ProductsList/>
     </>
   )
 }
